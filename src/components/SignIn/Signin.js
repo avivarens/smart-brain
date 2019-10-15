9924d7057dab82f8e6327e44cba7e4047ef79360
avivarens <42186995+avivarens@users.noreply.github.com>
2019-10-15 17:22:48 +0300

    replacing

--- src/components/SignIn/Signin.js
+++ src/components/SignIn/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('https://damp-gorge-74969.herokuapp.com/signin', {
+		fetch('https://murmuring-forest-56259.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
